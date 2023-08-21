cca84885e36dc8e3ea3b65f996a46a471d0abf7c
01manju <ngmanju07@gmail.com>
2023-08-21 15:32:44 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+        <p className="text-xl mt-20">
+               I am actively seeking an opportunity to apply my technical skills and become a valued member of an organization that fosters continuous learning. My goal is to excel in the field of Java Full Stack Software Development, where I have proficiency in Java, Springs, Servlet, SQL, JavaScript, as well as expertise in both Manual Testing and Automation Testing using Selenium. I am eager to contribute and grow within a dynamic team.
+        </p>
+
+        <br />
+
+       
+      </div>
+    </div>
+  );
+};
+
+export default About;
